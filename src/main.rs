077f3d3a76f7717e933dcdb7f8eef0dfdf1ebe7c
Wronskode <julienlahozpro@gmail.com>
2024-01-19 18:08:13 +0100

    Performances improvements

--- src/main.rs
+++ src/main.rs
@@ -27,12 +27,12 @@ pub fn main() {
     let mut canvas = window.into_canvas().build().unwrap();
     canvas.set_draw_color(Color::RGB(255, 255, 255));
     let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut i = 1;
+    let mut i = 2.0;
     let mut opt = true;
     let mut modified = true;
     let video = true;
-    let x_target = get_position(-0.5541669757014586, x_min, x_max, 0.0, width);
-    let y_target = get_position(0.6312605869248036, y_min, y_max, 0.0, height);
+    let mut x_target = get_position(-0.5541669757014586, x_min, x_max, 0.0, width);
+    let mut y_target = get_position(0.6312605869248036, y_min, y_max, 0.0, height);
     // println!("{}", compute_area(x_min, x_max, y_min, y_max));
     let mut s = 1.0;
     'running: loop {
@@ -46,21 +46,22 @@ pub fn main() {
                 | Event::MouseButtonDown { x, y, .. } => {
                     modified = true;
                     canvas.clear();
-                    let result = compute_zoom(width, height, x as f64, y as f64, x_min, x_max, y_min, y_max, (i+1) as f64);
+                    let result = compute_zoom(width, height, x as f64, y as f64, x_min, x_max, y_min, y_max, i);
                     x_min = result.0;
                     x_max = result.1;
                     y_min = result.2;
                     y_max = result.3;
-                    i += 1;
+                    x_target = result.4;
+                    y_target = result.5;
+                    i += 1.0;
                 }
                 | _ => {}
             }
         }
-        // The rest of the game loop goes here...
         if video {
             canvas.clear();
             s += 0.0001;
-            let result = compute_zoom(width, height, x_target as f64, y_target as f64, x_min, x_max, y_min, y_max, s);
+            let result = compute_zoom(width, height, x_target, y_target, x_min, x_max, y_min, y_max, s);
             x_min = result.0;
             x_max = result.1;
             y_min = result.2;
@@ -76,24 +77,26 @@ pub fn main() {
     }
 }
 
-fn get_position(x: f64, valeur_lue_min: f64, valeur_lue_max: f64, valeur_sortie_min: f64, valeur_sortie_max: f64) -> f64 {
-    (x-valeur_lue_min)*(valeur_sortie_max-valeur_sortie_min)/(valeur_lue_max-valeur_lue_min)+valeur_sortie_min
+fn get_position(x: f64, a: f64, b: f64, c: f64, d: f64) -> f64 {
+    (x-a)*(d-c)/(b-a)+c
 }
 
-fn compute_zoom(width: f64, height: f64, x: f64, y: f64, mut x_min: f64, mut x_max: f64, mut y_min: f64, mut y_max: f64, zoom_ratio: f64) -> (f64, f64, f64, f64) {
+fn compute_zoom(width: f64, height: f64, x: f64, y: f64, mut x_min: f64, mut x_max: f64, mut y_min: f64, mut y_max: f64, zoom_ratio: f64) -> (f64, f64, f64, f64, f64, f64) {
     let new_width = width/zoom_ratio;
     let new_height = height/zoom_ratio;
-    let r1 = x as f64 / (width as f64);
-    let r2 = y as f64 / (height as f64);
+    let r1 = x / width;
+    let r2 = y / height;
     let xmin = x_min;
     let xmax = x_max;
     let ymin = y_min;
     let ymax = y_max;
-    x_min = get_position((x as f64)-r1*new_width, 0.0, width, x_min, x_max);
-    y_min = get_position((y as f64)-r2*new_height, 0.0, height, y_min, y_max);
-    x_max = get_position((x as f64)-r1*new_width+new_width, 0.0, width, xmin, xmax);
-    y_max = get_position((y as f64)-r2*new_height+new_height, 0.0, height, ymin, ymax);
-    (x_min, x_max, y_min, y_max)
+    let x_target = x -r1*new_width;
+    let y_target = y -r2*new_height;
+    x_min = get_position(x_target, 0.0, width, x_min, x_max);
+    y_min = get_position(y_target, 0.0, height, y_min, y_max);
+    x_max = get_position(x -r1*new_width+new_width, 0.0, width, xmin, xmax);
+    y_max = get_position(y -r2*new_height+new_height, 0.0, height, ymin, ymax);
+    (x_min, x_max, y_min, y_max, x_target, y_target)
 }
 
 fn draw_mandelbrot_set(canvas: &mut Canvas<sdl2::video::Window>, iterations: u32, x1: f64, x2: f64, y1: f64, y2: f64, (w, h): (f64, f64), opt: bool) {
@@ -138,7 +141,6 @@ fn draw_mandelbrot_set(canvas: &mut Canvas<sdl2::video::Window>, iterations: u32
             }
             k += 1;
         }
-        
         k = 0;
         j += 1;
     }
